123a35c9713f97dba5af11c51f8d57b911038a42
cmzluna <claudiomazzoli@gmail.com>
2021-04-07 17:25:58 -0300

    form gets closed if  empty portion or on close btn get clicked

--- library.js
+++ library.js
@@ -1,20 +1,19 @@
 /*
-
 DOCS:
 https://css-tricks.com/snippets/css/a-guide-to-flexbox/
 
 Overlays
 https://tympanus.net/codrops/2013/11/07/css-overlay-techniques/
-
- 
 */
 
 const newBook = document.querySelector('#new-book');
 const formContainer = document.querySelector('#container');
 const overlay = document.querySelector('.overlay');
+const btnAgregar = document.querySelector('.boton-agregar');
+const form = document.getElementById('form');
+const bookshelf = document.querySelector(".bookshelf");
 
 let formOpen = false;
-
 let myLibrary = [];
 
 function Book(title, author, pages, readIt) {
@@ -41,17 +40,86 @@ function formOpenOrClosed() {
       }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(i) {
     // do stuff here
+    console.log(i);
+    
+    let bookNode = document.createElement("div");
+    bookNode.classList.add("book");
+    bookNode.setAttribute("data-index", `${i}`);
+   
+    const title = document.getElementById("title").value;
+    let titleNode = document.createElement("h2");
+    titleNode.innerHTML = `"${title}"`;
+  
+    const author = document.getElementById("author").value;
+    let authorNode = document.createElement("h3");
+    authorNode.innerHTML = `Author: ${author}`;
+  
+    const pages = document.getElementById("pages").value;
+    let pageNode = document.createElement("h3");
+    pageNode.innerHTML = `Pages: ${pages}`;
+  
+    const read = document.getElementById("read").value;
+    let readNode = document.createElement("h3");
+    readNode.innerHTML = `Read? ${read}${read === "Yes" ? "😃" : "😢"}`;
+  
+    let updateNode = document.createElement("button");
+    updateNode.classList = "update";
+    updateNode.innerHTML = `Update <i class="fas fa-pen"></i>`;
+  
+    let trashNode = document.createElement("button");
+    trashNode.classList = "trash";
+    trashNode.innerHTML = `Delete <i class="fas fa-trash-alt">`;
+  
+    bookNode.appendChild(titleNode);
+    bookNode.appendChild(authorNode);
+    bookNode.appendChild(pageNode);
+    bookNode.appendChild(readNode);
+    bookNode.appendChild(updateNode);
+    bookNode.appendChild(trashNode);
+    bookshelf.appendChild(bookNode);
+
+    const newBookInstance = new Book(title, author, pages, read);
+    formOpenOrClosed();
+    form.reset();
   }
 
 function displayBooks() {
     // display in a 'card' style
 }
 
+function resetForm() {
+
+}
+
 // new Book button
 // brings up a form allowing users to input the details for the new book: 
 // author, title, number of pages, whether it’s been read and anything else you might want.
 newBook.addEventListener('click', formOpenOrClosed);
+function logSubmit(event) {
+  log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+  event.preventDefault();
+}
 
-// button on each books card  to remove book from library 
+form.addEventListener("submit", (e, i) => {
+  e.preventDefault();
+  addBookToLibrary(i);
+}
+)
+// button on each books card to remove book from library 
+const bookSection = document.querySelector('.bookSection'); 
+
+// close form if clicked on the empty portion or on close btn
+bookSection.addEventListener('click', (e) => {
+  if (
+    e.target.className === 'bookSection' ||
+    e.target.classList.contains('close-modal')
+  ) {
+    // can't reuse formOpenOrClosed because clicking in bookSection would trigger opening the modal again!
+    formContainer.style.transform = "scale(0)";
+        overlay.style.opacity = 0;
+        newBook.style.transform = "rotate(0deg)";
+        formOpen = false;
+  }
+});
