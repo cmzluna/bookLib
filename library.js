006e6f8b7f52e4b5cabfee18d5b4e03bce9a8862
cmzluna <claudiomazzoli@gmail.com>
2021-04-07 19:34:24 -0300

    added delete button func

--- library.js
+++ library.js
@@ -41,49 +41,63 @@ function formOpenOrClosed() {
 }
 
 function addBookToLibrary(i) {
-    // do stuff here
-    console.log(i);
-    
-    let bookNode = document.createElement("div");
-    bookNode.classList.add("book");
-    bookNode.setAttribute("data-index", `${i}`);
-   
-    const title = document.getElementById("title").value;
-    let titleNode = document.createElement("h2");
-    titleNode.innerHTML = `"${title}"`;
-  
-    const author = document.getElementById("author").value;
-    let authorNode = document.createElement("h3");
-    authorNode.innerHTML = `Author: ${author}`;
-  
-    const pages = document.getElementById("pages").value;
-    let pageNode = document.createElement("h3");
-    pageNode.innerHTML = `Pages: ${pages}`;
-  
-    const read = document.getElementById("read").value;
-    let readNode = document.createElement("h3");
-    readNode.innerHTML = `Read? ${read}${read === "Yes" ? "😃" : "😢"}`;
-  
-    let updateNode = document.createElement("button");
-    updateNode.classList = "update";
-    updateNode.innerHTML = `Update <i class="fas fa-pen"></i>`;
-  
-    let trashNode = document.createElement("button");
-    trashNode.classList = "trash";
-    trashNode.innerHTML = `Delete <i class="fas fa-trash-alt">`;
-  
-    bookNode.appendChild(titleNode);
-    bookNode.appendChild(authorNode);
-    bookNode.appendChild(pageNode);
-    bookNode.appendChild(readNode);
-    bookNode.appendChild(updateNode);
-    bookNode.appendChild(trashNode);
-    bookshelf.appendChild(bookNode);
+  // do stuff here
+  console.log(i);
 
-    const newBookInstance = new Book(title, author, pages, read);
-    formOpenOrClosed();
-    form.reset();
-  }
+  let bookNode = document.createElement("div");
+  bookNode.classList.add("book");
+ 
+  const title = document.getElementById("title").value;
+  let titleNode = document.createElement("h2");
+  titleNode.innerHTML = `"${title}"`;
+
+  const author = document.getElementById("author").value;
+  let authorNode = document.createElement("h3");
+  authorNode.innerHTML = `Author: ${author}`;
+
+  const pages = document.getElementById("pages").value;
+  let pageNode = document.createElement("h3");
+  pageNode.innerHTML = `Pages: ${pages}`;
+
+  const read = document.getElementById("read").value;
+  let readNode = document.createElement("h3");
+  readNode.innerHTML = `Read? ${read}${read === "Yes" ? "😃" : "😢"}`;
+
+  // create Book instance 
+  const newBookInstance = new Book(title, author, pages, read);
+  myLibrary.push(newBookInstance);
+
+  // update data-index once instance has been given an index ID
+  // I don't need this in fact as the index is being given in the trashnode Eventhandler
+  //bookNode.setAttribute("data-index", myLibrary.indexOf(newBookInstance));
+
+  let updateNode = document.createElement("button");
+  updateNode.classList = "update";
+  updateNode.innerHTML = `Update <i class="fas fa-pen"></i>`;
+
+  let trashNode = document.createElement("button");
+  trashNode.classList = "trash";
+  trashNode.innerHTML = `Delete <i class="fas fa-trash-alt">`;
+
+  bookNode.appendChild(titleNode);
+  bookNode.appendChild(authorNode);
+  bookNode.appendChild(pageNode);
+  bookNode.appendChild(readNode);
+  bookNode.appendChild(updateNode);
+  bookNode.appendChild(trashNode);
+  bookshelf.appendChild(bookNode);
+
+  console.log(bookNode);
+
+  formOpenOrClosed();
+  form.reset();
+
+  trashNode.addEventListener("click", () => {
+    bookshelf.removeChild(bookNode);
+    myLibrary.splice(myLibrary.indexOf(newBookInstance), 1);  // no funciona borrando bien el item 
+  });
+
+}
 
 function displayBooks() {
     // display in a 'card' style
@@ -105,6 +119,7 @@ function logSubmit(event) {
 form.addEventListener("submit", (e, i) => {
   e.preventDefault();
   addBookToLibrary(i);
+  console.log(i);
 }
 )
 // button on each books card to remove book from library 
@@ -123,3 +138,4 @@ bookSection.addEventListener('click', (e) => {
         formOpen = false;
   }
 });
+
